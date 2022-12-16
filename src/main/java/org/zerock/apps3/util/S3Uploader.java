c8d58998526913621b0603ada27f880a1f468f03
SungYong Lee <Saintdragon2@gmail.com>
2022-12-16 14:18:40 +0900

    Initial commit

---
+++ src/main/java/org/zerock/apps3/util/S3Uploader.java
@@ -0,0 +1,64 @@
+package org.zerock.apps3.util;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+public class S3Uploader {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    public String bucket;
+
+    //S3로 파일 업로드하기
+    public String upload(String filePath) throws RuntimeException{
+
+        File targetFile = new File(filePath);
+
+        String uploadImageUrl = putS3(targetFile, targetFile.getName());
+
+        removeOriginalFile(targetFile);
+
+        return uploadImageUrl;
+    }
+
+
+
+    //S3로 업로드
+    private String putS3(File uploadFile, String fileName) {
+
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    //S3 업로드 후 원본파일 삭제
+    private void removeOriginalFile(File targetFile){
+        if(targetFile.exists() && targetFile.delete()){
+            log.info("File delete suceess");
+            return;
+        }
+        log.info("fail to remove");
+    }
+
+
+    public void removeS3File(String fileName){
+        final DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(bucket, fileName);
+        amazonS3Client.deleteObject(deleteObjectRequest);
+    }
+
+
+
+}
